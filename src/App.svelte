6b782cf739cd2c645acd6172ed9592df8c934f6e
Reski Mulud Muchamad <reski.mulud@gmail.com>
2021-05-07 02:58:23 +0700

    commit pertama aplikasi todo-app menggunakan svelte

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import TodoCard from "./TodoCard.svelte";
+</script>
+
+<div class="container is-fluid">
+	<h1 class="is-size-3">Todp App</h1>
+	<div class="columns">
+		<div class="column is-4">
+			<div class="card has-background-light">
+				<div class="card-header">
+					<p class="card-header-title">Tasks</p>
+				</div>
+				<div class="card-content">
+					<TodoCard content={"Belajar HTML"} >
+					<TodoCard content={"Belajar CSS"} >
+					<TodoCard content={"Belajar JavaScript"} >
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<svelte:head>
+	<link rel="stylesheet" href="/bulma.min.css" />
+</svelte:head>
---
+++ src/TodoCard.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let content;
+</script>
+
+<div class="card mb-3 has-background-primary-light">
+    <div class="card-content">
+        <p class="has-text-primary-dark">{content}</p>
+    </div>
+</div>
